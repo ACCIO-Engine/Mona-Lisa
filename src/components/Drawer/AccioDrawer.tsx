cc47bb5558a6217cec5c07c5417f118bcc37dbe2
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2024-03-11 11:17:44 +0200

    clean: remove redundancy

--- src/components/Drawer/AccioDrawer.tsx
+++ src/components/Drawer/AccioDrawer.tsx
@@ -7,8 +7,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import Divider from "@mui/material/Divider";
 import { CustomDrawer, DrawerHeader } from "./AccioDrawer.styled";
 import DashboardIcon from "@mui/icons-material/Dashboard";
