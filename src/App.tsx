7b21042fd2c31215298ab47bc83cd0521db07744
Fady Adel <75928317+fady2001@users.noreply.github.com>
2024-03-11 21:00:22 +0200

    merge routs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { lightTheme, darkTheme } from "./theme";
 import { ThemeProvider } from "@emotion/react";
 import { Box, CssBaseline } from "@mui/material";
@@ -7,8 +8,27 @@ import React from "react";
 import { DrawerHeader } from "./components/Drawer/AccioDrawer.styled";
 import Settings from "./layouts/Settings/Settings";
 import DialogProvider from "../src/components/Dialog/Dialog";
+import Root from "./layouts/Root/Root";
+import Home from "./pages/Home/Home";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        // index is true to make it the default route
+        index: true,
+        element: <Home />,
+      },
+      // { path: "search", element: <Search /> },
+      // { path: "index", element: <Index /> },
+      // { path: "settings", element: <Settings /> },
+    ],
+  },
+]);
+
+function App(): React.ReactElement {
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
@@ -27,6 +47,8 @@ function App() {
   return (
     <ThemeProvider theme={isLightMode ? lightTheme : darkTheme}>
       <CssBaseline />
+
+      <RouterProvider router={router} />
       <Box sx={{ display: "flex" }}>
         <DialogProvider>
           <Navbar
