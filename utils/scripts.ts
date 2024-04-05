5db4886c45e1a3beeedabd6adbde0039c9c9281d
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2024-04-05 13:23:01 +0200

    feat: kill the running process

--- utils/scripts.ts
+++ utils/scripts.ts
@@ -1,46 +1,44 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { exec } = require('child_process')
-const path = require('node:path')
+const { spawn } = require('node:child_process')
+const path = require('path')
 
 process.env.HEDWIG = path.join(__dirname, '../../Hedwig')
 
-// Function to run shell command
 function runShellCommand (command: string): any {
-  // Executing the command
-  const child = exec(command)
-
-  // Handling standard output
-  child.stdout?.on('data', (data: Buffer) => {
-    console.log(`stdout: ${data.toString()}`)
+  const child = spawn(command, {
+    // stdio: 'inherit',
+    shell: true,
+    cwd: process.env.HEDWIG,
+    killSignal: 'SIGINT'
   })
 
-  // Handling error output
-  child.stderr?.on('data', (data: Buffer) => {
-    console.error(`stderr: ${data.toString()}`)
+  child.stdout.on('data', (data: any) => {
+    console.log(`${data}`)
   })
 
-  // Handling process close event
+  child.stderr.on('data', (data: any) => {
+    console.error(`${data}`)
+  })
+
   child.on('close', (code: number) => {
     console.log(`child process exited with code ${code}`)
   })
-
   return child
 }
 
-const test = runShellCommand(`cd ${process.env.HEDWIG} && python ./server.py`)
+const test = runShellCommand('python server.py')
 
 function stopShellCommand (child: any): void {
   console.log('Stopping the command')
   if (child != null) {
     console.log('Killing the child process')
-    child.kill()
+    spawn('taskkill', ['/pid', child.pid, '/f', '/t'])
   }
 }
-// Exporting the function
 
-// Stop the command
 setTimeout(() => {
   stopShellCommand(test)
-}, 20000)
+  console.log('Command stopped')
+}, 10000)
 
 module.exports = runShellCommand
