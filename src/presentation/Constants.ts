191280e4ad604bafbb751a0c334b700840160b42
Abd-ELrahmanHamza <a.m.hamza156@gmail.com>
2024-07-11 03:14:44 +0300

    change slogan

--- src/presentation/Constants.ts
+++ src/presentation/Constants.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "Accio";
-export const SLOGAN = "Where Every File is Just a Search Away";
+export const SLOGAN = "Find what you need, the way you think";
 
 export const IPC_EVENTS = {
   READ_FILE: "read-file"
